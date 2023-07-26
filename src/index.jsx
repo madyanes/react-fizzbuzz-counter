2902b6b96530fdbe171500ff42167314f40bf286
madyanes <madyanes.dev@gmail.com>
2023-07-27 01:36:39 +0700

    create state and event handlers

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,31 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 
 class CounterApp extends React.Component {
+    constructor(props) {
+        super(props)
+
+        // inisialisasi nilai count di dalam state
+        this.state = {
+            count: 0,
+        }
+    }
+
+    onIncreaseEventHandler() {
+        this.setState((previousState) => {
+            return {
+                count: previousState.count + 1
+            }
+        })
+    }
+
+    onResetEventHandler() {
+        this.setState(() => {
+            return {
+                count: 0
+            }
+        })
+    }
+
     render() {
         return (
             <div>
