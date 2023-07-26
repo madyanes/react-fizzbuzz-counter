24499f337ef7b4386f6d20ee8a5efc0f637e2360
madyanes <madyanes.dev@gmail.com>
2023-07-27 01:38:15 +0700

    create Counter Display component

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 
+const CounterDisplay = ({ count }) => {
+    return <p>{count}</p>
+}
+
 class CounterApp extends React.Component {
     constructor(props) {
         super(props)
