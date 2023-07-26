33efbec08477622deb6dd7fecdad51d8027b546a
madyanes <madyanes.dev@gmail.com>
2023-07-27 01:53:47 +0700

    write fizz-buzz logic

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,19 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 
 const CounterDisplay = ({ count }) => {
+    if (count === 0)
+        return <p>{count}</p>
+
+    if (count % 5 === 0 && count % 7 === 0)
+        return <p>FizzBuzz</p>
+
+    if (count % 5 === 0) {
+        return <p>Fizz</p>
+    }
+
+    if (count % 7 === 0)
+        return <p>Buzz</p>
+
     return <p>{count}</p>
 }
 
